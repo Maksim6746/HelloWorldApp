c8bad6a69bd439b39712b168245c4388d74c93cb
Maxim Migranov <maksim.migranov@mail.ru>
2022-07-31 00:21:20 +0500

    Второе задание Светофор

--- HelloWorldApp/ViewController.swift
+++ HelloWorldApp/ViewController.swift
@@ -9,37 +9,64 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var greetingLabel: UILabel!
-    @IBOutlet var greetingButton: UIButton!
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    enum TrafficLightSignals {
+        case red, yeelow, green
+    }
+    
+    private var trafficLightSignals = TrafficLightSignals.red
+    private let lightOff: CGFloat = 0.3
+    private let lightOn: CGFloat = 1
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        greetingLabel.isHidden = true
-//        greetingButton.layer.cornerRadius = 10
-        greetingButton.configuration = setupButton(with: "Show Greeting")
-
+        
+        startButton.layer.cornerRadius = 10
+        
+        redLight.layer.cornerRadius = redLight.frame.size.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.size.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.size.width / 2
+        
+        redLight.alpha = lightOff
+        yellowLight.alpha = lightOff
+        greenLight.alpha = lightOff
+        
     }
 
-    @IBAction func greetingButtonPressed() {
-        greetingLabel.isHidden.toggle()
+    @IBAction func startButtonClick() {
+        startButton.setTitle("NEXT", for: .normal)
         
-        greetingButton.configuration = setupButton(with: greetingLabel.isHidden ? "Show Greeting" : "Hide Greeting")
-        
-//        if greetingLabel.isHidden {
-//            greetingButton.setTitle("Show Greeting", for: .normal)
-//        } else {
-//            greetingButton.setTitle("Hide Greeting", for: .normal)
-//        }
+        switch trafficLightSignals {
+        case .red:
+            yellowLight.alpha = lightOff
+            greenLight.alpha = lightOff
+            redLight.alpha = lightOn
+            trafficLightSignals = .yeelow
+        case .yeelow:
+            redLight.alpha = lightOff
+            greenLight.alpha = lightOff
+            yellowLight.alpha = lightOn
+            trafficLightSignals = .green
+        case .green:
+            redLight.alpha = lightOff
+            yellowLight.alpha = lightOff
+            greenLight.alpha = lightOn
+            trafficLightSignals = .red
+        }
+        if greenLight.alpha == lightOn {
+            startButton.setTitle("RESTART", for: .normal)
+        }
     }
     
-    private func setupButton(with title: String) -> UIButton.Configuration {
-        var buttonConfiguration = UIButton.Configuration.filled()
-        buttonConfiguration.baseBackgroundColor = #colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1)
-        buttonConfiguration.title = title
-        buttonConfiguration.buttonSize = .large
-        buttonConfiguration.cornerStyle = .large
-        buttonConfiguration.attributedTitle?.font = UIFont.systemFont(ofSize: 24)
-        return buttonConfiguration
-    }
 }
+
+    
+
 
